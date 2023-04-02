7d89f27e0dd256bd2ea832bcf3b5fac85517912c
fffadhilll <nurmuhammadfadhilah2@gmail.com>
2023-04-02 14:18:47 +0700

    fix auth admin and user

--- resources/views/layouts/navbar-admin.blade.php
+++ resources/views/layouts/navbar-admin.blade.php
@@ -17,8 +17,13 @@
             <li class="nav-item dropdown no-arrow">
                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><img class="border rounded-circle img-profile" src="{{ asset('assets/admin/images/avatar1.jpeg') }}"></a>
                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
-                        <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button class="dropdown-item" type="submit" class="dropdown-item"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>Logout</button>
+                        </form>
+                        {{-- <a class="dropdown-item" href="{{ route('logout') }}"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a> --}}
                     </div>
+                    
                 </div>
             </li>
         </ul><span style="font-weight: bold;">Profile</span>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,20 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [UserController::class, 'landingPageView']);
 
-Route::get('/login', [UserController::class, 'loginView'])->name('login');
+Route::get('/login', [UserController::class, 'loginView'])->name('login')->middleware('guest');
 Route::post('/login', [UserController::class, 'loginUser']);
 
-Route::get('/register', [UserController::class, 'registerView'])->name('register');
+Route::get('/register', [UserController::class, 'registerView'])->middleware('guest')->name('register');
 Route::post('/register', [UserController::class, 'registerUser']);
 
-Route::post('/logout', [UserController::class, 'logoutUser']);
+Route::post('/logout', [UserController::class, 'logoutUser'])->name('logout');
+
+Route::get('/', [UserController::class, 'landingPageView']);
 
 Route::prefix('/admin')->group(function () {
-    Route::get('/', [AdminController::class, 'dashboardView'])->name('dashboard');
-    Route::get('/listProduct', [AdminController::class, 'showProducts'])->name('listProduct');
-    Route::get('/addProduct', [AdminController::class, 'addProductView'])->name('addProduct');
-    Route::post('/saveProduct', [ProductController::class, 'saveProduct'])->name('saveProduct');
-    Route::delete('/deleteProduct/{id}', [ProductController::class, 'deleteProduct'])->name('deleteProduct');
+    Route::get('/listProduct', [AdminController::class, 'showProducts'])->middleware('auth')->name('listProduct');
+    Route::get('/addProduct', [AdminController::class, 'addProductView'])->middleware('auth')->name('addProduct');
+    Route::post('/saveProduct', [ProductController::class, 'saveProduct'])->middleware('auth')->name('saveProduct');
+    Route::delete('/deleteProduct/{id}', [ProductController::class, 'deleteProduct'])->middleware('auth')->name('deleteProduct');
+    Route::get('/', [AdminController::class, 'dashboardView'])->name('dashboard')->middleware('auth');
 });
